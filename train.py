4d2a725ab8d0259038dedfee71f768312146136b
Daniel Whitenack <whitenack.daniel@gmail.com>
2022-06-14 11:14:39 -0400

    changes for Nathan example

--- train.py
+++ train.py
@@ -65,13 +65,13 @@ if __name__ == '__main__':
     # ClearML stuff
     Task.add_requirements("-rrequirements.txt")
     task = Task.init(
-      project_name='ClearML-AQuA-Demo',    # project name of at least 3 characters
+      project_name='IDX_Nathan_Internship',    # project name of at least 3 characters
       task_name='squad-fine-tuning-' + str(int(time.time())), # task name of at least 3 characters
       task_type="training",
       tags=None,
       reuse_last_task_id=True,
       continue_last_task=False,
-      output_uri="s3://aqua-idx/squad-ex",
+      output_uri="s3://nathan-internship/clearml-artifacts",
       auto_connect_arg_parser=True,
       auto_connect_frameworks=True,
       auto_resource_monitoring=True,
